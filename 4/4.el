dbbaec548e352956fc0411a96b31aef0b946b485
david <davidrmarques@protonmail.com>
2018-09-30 15:33:04 +0100

    Number 4

--- 4/4.el
+++ 4/4.el
@@ -2,25 +2,41 @@
 ;; The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 ;; Find the largest palindrome made from the product of two 3-digit numbers.
 
+
 (defun is-palindromic (n)
   "Returns bool"
+  ;; Converts to string
+  (if (string= n (reverse n))
+      t ;; If
+      nil ;; Else
+      )
+  )
 
-     )
-
-(defun 4-euler ()
-  (let ((n1 999)(n2 999)(MAX 0)(ph 0))
-  
+(defun 4-euler (m)
+  ;; Local variables
+  (let ((n1 m)(n2 m)(MAX 0)(ph 0))
 
   ;; Double while loop like a brainlet
   (while (> n1 0)
+    (setq n2 m)
     (while (> n2 0)
 
       (setq ph (* n2 n1))
-      (if (and ( > ph MAX) (is-palidromic ph))
-          (setq MAX ph)
-          )
+
+      ;; If it is palindromic
+      (if (and ( > ph MAX) (is-palindromic (format "%d" ph)))
+          (setq MAX ph))
+
+      (setq n2 (- n2 1))
       )
+    (setq n1 (- n1 1))
     )
+
+  ;; Adds solution to kill-ring 
   (kill-new (format "%d" MAX))
+
    ) 
   )
+
+(4-euler 999)
+
