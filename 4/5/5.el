fc77d48b29ea84bc808291fcda99de7cf3a2b651
david <davidrmarques@protonmail.com>
2018-09-30 15:58:14 +0100

    Finished problem 5

--- 4/5/5.el
+++ 4/5/5.el
@@ -1,4 +1,25 @@
 ;; 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 ;; What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
+(defun check (n)
+  (let ((l 1)(flag t))
+    (while (and (<= l  20) flag)
+      (if (= (% n l) 0) (setq l (+ 1 l)) (setq flag nil))
+      )
+    flag
+    )
+  )
 
+;; (check 252)
+
+
+(defun 5-euler ()
+  (let ((i 1) (flag t)) ;; i is a counter
+    (while flag
+      (if (check i) (setq flag nil) (setq i (+ i 1)))
+      )
+    (kill-new (format "%d" i))
+    )
+  )
+   
+(5-euler)
