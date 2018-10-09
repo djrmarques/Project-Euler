0a2498ce40841a12f14b16f324873836ec7a8336
David <davidrmarques@protonmail.com>
2018-10-09 14:17:00 +0100

    Finished project euler 12 elisp

--- Elisp/12/12.el
+++ Elisp/12/12.el
@@ -11,30 +11,77 @@
 ;; We can see that 28 is the first triangle number to have over five divisors.
 ;; What is the value of the first triangle number to have over five hundred divisors?
 
+;; The number of divisors of a number, N, is: (a+1)(b+1)... where a,b.. are the exponents of the prime divisors of the number
+
+(defun list-factorize(n)
+  "Returns a list of the facotrizied numbers"
+  (let ((i 1) (buff ()))
+    (while (< i n)
+      (setq i (+ 1 i))
+      (while (= (% n i) 0)
+        (setq buff (cons i buff))
+        (setq n (/ n i))
+        )
+      )
+      (reverse buff)
+    )
+)
+
+;; (list-factorize 28)                     
+
+(defun uniques-list (list)
+  "Returns list of unique items"
+(let ((l 0)(al ()))
+  (dolist (a list al)
+    (if (not (member a al))
+        (setq al (cons a al))
+        )
+    )
+  al
+  )
+)
+
+;; (uniques-list '(1 2 3 4 1 2 312 31))
+
+(defun number-elements (el fact-list)
+  "Return the number of elements el in fact-list"
+  (let ((i 0))
+    (dolist (e fact-list i)
+      (if (= e el) (setq i (+ i 1)))
+      )
+    i
+      )
+    )
 
 (defun get-n-div (n)
-  (let ((j 1) (n-div 1))
-    (while (<= j (/ n 2))
-      (if (= (% n j) 0 ) (setq n-div (+ 1 n-div)))
-      (setq j (+ 1 j))
+  "Returns the number of divisors."
+  (let ((fact-list (list-factorize n)) unique-fact)
+    (setq unique-fact (uniques-list fact-list))
+    (let ((div-count 1))
+      (dolist (el unique-fact div-count)
+        (setq div-count(* div-count (+ 1 (number-elements el fact-list))))
+        )
+    div-count
       )
-    n-div
     )
   )
 
-;; (get-n-div 28)
+(list-factorize  76576500)
+(get-n-div 76576500)
 
 (defun euler-12 (max)
-  (let ((i 0) (N 0) (n-div 0) (step 100))
+  (let ((i 0) (N 0) (n-div 0) (step 10000))
     (while (<= n-div max)
       (if (= (% i step) 0) (print (format "i:%d N:%d n-div:%d" i N n-div)))
       (setq N (+ i N))
       (setq i (+ 1 i))
       (setq n-div (get-n-div N))
       )
-    (kill-new (format "%d" n))
-    (write-region (format "%d\n" n) nil "ans" 'append)
+    (kill-new (format "%d" N))
+    (write-region (format "%d\n" N) nil "ans" 'append)
+    N
   )
 )
-
 (euler-12 500)
+
+
