6a6f37885d507cd347985eeee69c9a9348b7a9ef
David <davidrmarques@protonmail.com>
2018-10-12 20:44:51 +0100

    Finished 16 ELISP

--- Elisp/16/16.el
+++ Elisp/16/16.el
@@ -1,21 +1,44 @@
 ;; 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 ;; What is the sum of the digits of the number 2^1000?
 
+
+
 (defun sum-digits (d)
   "Returns the sum of the digits"
+  (let ((i 0) (sum 0))
+    (while (< i (length d))
+      ;; (print (aref d i))
+      (setq sum (+ sum (- (aref d i) 48)))
+      (setq i (+ 1 i))
+    )
+    sum
   )
+)
 
-(defun euler-16(n)
-  (let ((i 0) (v 1) (sum 2))
-    (while (< i n)
-      (setq v (* v 2))
+;; (sum-digits "5555")
+
+(defun power-sum (e)
+    ;; POWER
+  (let ((i 1) (v "2"))
+    (while (< i e)
+      (setq v  (calc-eval (format "%s * 2" v)))
       (setq i (+ 1 i))
       )
+    (sum-digits v)
+    )
+  )
 
-    ;; Calculate the sum of the digits
+
+(defun euler-16(n)
+  (let (v)
+    (setq v (power-sum n))
+    (kill-new (format "%d" v))
+    (print v)
     )
   )
 
 (euler-16 1000)
+
+
 
 
